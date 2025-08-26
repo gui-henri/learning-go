21ba2a9ba4dcbbe505aac0ae2cecf202f215e6a2
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-08-26 13:02:00 -0300

    first commit

---
+++ backend/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type NovaTarefaInput struct {
+	Descricao string `json:"descricao"`
+	Prazo     string `json:"prazo"`
+}
+
+type Tarefa struct {
+	Descricao string `json:"descricao"`
+	Prazo     string `json:"prazo"`
+	Concluida bool   `json:"concluida"`
+	CriadaEm  string `json:"criada-em"`
+}
+
+type NovaTarefaOutput struct {
+	Descricao string `json:"descricao"`
+	Prazo     string `json:"prazo"`
+	Concluida bool   `json:"concluida"`
+	CriadaEm  string `json:"criada-em"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /task", func(w http.ResponseWriter, r *http.Request) {
+
+		var novaTarefaInput NovaTarefaInput
+		var novaTarefaOutput NovaTarefaOutput
+		err := json.NewDecoder(r.Body).Decode(&novaTarefaInput)
+
+		if err != nil {
+			http.Error(w, "Requisição não pôde ser lida", http.StatusBadRequest)
+		}
+
+		novaTarefaOutput = NovaTarefaOutput{
+			Descricao: novaTarefaOutput.Descricao,
+			Prazo:     novaTarefaInput.Prazo,
+			Concluida: false,
+			CriadaEm:  time.Now().Format(time.RFC850),
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(novaTarefaOutput)
+
+	})
+
+	mux.HandleFunc("GET /task/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "handling task with id=%v\n", id)
+	})
+
+	http.ListenAndServe("localhost:8090", mux)
+}
