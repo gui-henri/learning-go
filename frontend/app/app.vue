4b6c8a169b03f6b48e2d5b95d153b9e83c38052a
Samuel Vitor Melo do Nascimento <samuel.nascimento@interne.com.br>
2025-08-28 15:22:59 -0300

    feat/front-end build

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,53 @@
+<!-- frontend/app/app.vue -->
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="logo">
+      <a href="https://interne.com.br" target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://cdn.bitrix24.com.br/b22035513/landing/2a4/2a44b7c2b4e7e248ec783a2eae611962/Logo_Interne_180_x_60_px_1x.png"
+          alt="Logo Interne"
+        />
+      </a>
+    </div>
+
+    <nav class="barra-nav">
+      <NuxtLink to="/" class="nav-link">Página inicial</NuxtLink>
+      <NuxtLink to="/equipe" class="nav-link">Equipe</NuxtLink>
+      <NuxtLink to="/proposta" class="nav-link">Nossa proposta</NuxtLink>
+      <NuxtLink to="/tarefas" class="nav-link">Visualizar tarefas</NuxtLink>
+    </nav>
+
+    <!-- Aqui é onde será renderizado o conteúdo da rota -->
+    <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.logo {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.barra-nav {
+  display: flex;
+  background-color: red;
+  height: 40px;
+  width: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.nav-link {
+  flex: 1;
+  text-align: center;
+  color: white;
+  text-decoration: none;
+  padding: 10px 0;
+  transition: background-color 0.3s;
+}
+
+.nav-link:hover {
+  background-color: darkred;
+}
+</style>
