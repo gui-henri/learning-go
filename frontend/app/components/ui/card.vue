cd9079be1c9034020d63a51180b587e0f4bee846
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-08-28 16:32:16 -0300

    feat: fetch data on frontend

---
+++ frontend/app/components/ui/card.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card-body">
+        <div>
+            <h3>{{ descricao }}</h3>
+            <h4>{{ criadaEm }}</h4>
+            <p>{{ concluida ? "Concluída" : "Em andamento" }}</p>
+        </div>
+        <div>
+            <button>Concluir</button>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    .card-body {
+        display: flex;
+        align-items: center;
+        width: 300px;
+        justify-content: space-between;
+    }
+
+    h3, h4, p {
+        padding: 0;
+        margin: 0;
+    }
+</style>
+
+<script setup>
+defineProps({
+  descricao: String,
+  criadaEm: String,
+  concluida: Boolean
+})
+</script>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+    const { data, pending, error } = await useFetch('http://backend:8090/task/all-active')
+</script>
+
 <template>
     INPUT DE NOVAS TAREFAS
     ----------------
-    TAREFAS ABERTAS
+    <div v-if="pending">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
+    <div>
+        <UiCard v-for="task in data" :key="task.id" :descricao="task.descricao" :criadaEm="task['criada-em']" :concluida="task.concluida" />
+    </div>
 </template>
