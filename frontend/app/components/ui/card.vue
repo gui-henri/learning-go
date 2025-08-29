ca20974497523decad1dff4abebc80a49cd31b80
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-08-29 12:01:24 -0300

    feat: saving progress on cards

--- frontend/app/components/ui/card.vue
+++ frontend/app/components/ui/card.vue
@@ -13,10 +13,14 @@
 
 <style lang="css" scoped>
     .card-body {
+        width: 100%;
+        max-width: 520px;
         display: flex;
         align-items: center;
-        width: 300px;
         justify-content: space-between;
+
+        border: 2px solid black;
+        box-shadow: 2px 2px 10px gray;
     }
 
     h3, h4, p {
---
+++ frontend/app/components/ui/taskInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="text" name="description" id="" placeholder="Digite a tarefa">
+        <button>Enviar</button>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    
+    div {
+        display: flex;
+        width: 100%;
+        max-width: 520px;
+    }
+    input {
+        width: 100%;
+    }
+
+</style>
