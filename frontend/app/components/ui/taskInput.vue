e860f8e4c7d06b04df2aa7f98fa9932f96cd3161
Guilherme Henrique <guihenrique.dev@gmail.com>
2025-09-09 14:47:26 -0300

    feat: sendTasks

--- frontend/app/components/ui/taskInput.vue
+++ frontend/app/components/ui/taskInput.vue
@@ -1,7 +1,31 @@
+<script setup lang="ts">
+    const config = useRuntimeConfig();
+    const descricao = ref('');
+
+    async function sendTask() {
+        try {
+            const response = await fetch(config.apiBase + "/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ descricao: descricao.value, prazo: "amanhã" })
+            })
+
+            if (!response.ok) {
+                throw new Error("Resposta diferente de ok")
+            }
+
+        } catch (error) {
+            console.log("Erro ao realizar requisição", error)
+        }
+    }
+</script>
+
 <template>
     <div class="items-center justify-center gap-2">
-        <Input class="p-2 rounded-full border-2 border-red-500" type="text" name="description" placeholder="Digite a tarefa" />
-        <Button class="bg-red-600 hover:bg-red-500 p-5 rounded-3xl">Enviar</Button>
+        <Input v-model="descricao" class="p-2 rounded-full border-2 border-red-500" type="text" name="description" placeholder="Digite a tarefa" />
+        <Button @click="sendTask" class="bg-red-600 hover:bg-red-500 p-5 rounded-3xl">Enviar</Button>
     </div>
 </template>
 
