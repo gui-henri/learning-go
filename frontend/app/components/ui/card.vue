6c27891f41ae8f75d2f81a12f28ae6167b63e37d
Samuel <samuelvitor224@gmail.com>
2025-09-10 16:38:55 -0300

    overall fix

--- frontend/app/components/ui/card.vue
+++ frontend/app/components/ui/card.vue
@@ -35,7 +35,7 @@
             }).format(new Date(props.prazo))
         } catch (err) {
             console.error("Invalid criadaEm date:", props.criadaEm, err)
-            return "Data inválida"
+            return "Indefinido"
         }
     })()
 
@@ -64,7 +64,7 @@
     <div class="card-body pl-3 pr-3 pt-2 pb-2 rounded-2xl border-2 border-b-red-600 border-l-red-400 border-r-red-600 border-t-red-400">
         <div>
             <h3 class="font-bold text-xl">{{ descricao }}</h3>
-            <p>Prazo: {{ formatedPrazo }}</p>
+            <p>{{ formatedPrazo === "Indefinido" ? "Sem prazo" : formatedPrazo}}</p>
             <h4>Criada em: {{ formattedDate }}</h4>
         </div>
         <div class="gap-2 flex">
