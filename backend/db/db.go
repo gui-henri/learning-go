5292907beffb6e2680766a5e1a0f33adae978798
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-09-12 16:16:12 -0300

    save: starting tests

---
+++ backend/db/db.go
@@ -0,0 +1,14 @@
+package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type IDB interface {
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+}
