24a5a63b987c84965da79b9dccaa1bf71cfa0396
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-09-12 17:53:44 -0300

    feat: early sql

--- backend/db/migrations/create_tarefas.sql
+++ backend/db/migrations/create_tarefas.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS tasks (
   criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS pacientes (
+CREATE TABLE IF NOT EXISTS patient (
  id SERIAL PRIMARY KEY,
  version INT DEFAULT 0,
  last_updated TIMESTAMP,
@@ -26,6 +26,38 @@ CREATE TABLE IF NOT EXISTS pacientes (
  builder_id VARCHAR,
  upid VARCHAR,
  created_at VARCHAR,
+ resource_json JSON,
+ data_source VARCHAR,
+)
+
+CREATE TABLE IF NOT EXISTS identifier(
+id PRIMARY KEY,
+system VARCHAR,
+value VARCHAR,
+use VARCHAR,
+type_display VARCHAR,
+type_code VARCHAR,
+assigner_display VARCHAR,
+)
+
+CREATE TABLE ORGANIZATION (
+    id VARCHAR PRIMARY KEY,
+    version INT,
+    last_updated TIMESTAMP,
+    name VARCHAR,
+    address_line_1 VARCHAR,
+    address_line_2 VARCHAR,
+    address_city VARCHAR,
+    address_state VARCHAR,
+    address_postal_code VARCHAR,
+    builder_id VARCHAR,
+    created_at TIMESTAMP,
+    resource_json VARIANT,
+    data_source VARCHAR,
+);
+
+
+
 
 
 
