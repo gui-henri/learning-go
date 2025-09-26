e28808ed245a2934c72fe70ad2f6383e3e86b7fe
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-09-26 18:11:34 -0300

    feat: starting service

--- backend/internal/patient/service.go
+++ backend/internal/patient/service.go
@@ -2,6 +2,9 @@ package patient
 
 import (
 	"context"
+	"math"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/gui-henri/learning-go/pkg/fhir"
@@ -10,6 +13,7 @@ import (
 
 type PatientService interface {
 	InsertPatient(ctx context.Context, p fhir.Patient) (paciente, error)
+	ListPatients(ctx context.Context, count, page int) (fhir.Bundle, error)
 }
 
 type patientService struct {
@@ -20,6 +24,17 @@ func NewPatientService(r patientRepository) *patientService {
 	return &patientService{
 		repository: r,
 	}
+}
+
+func buildLinkURL(baseURL string, count, offset int) string {
+	u, _ := url.Parse(baseURL)
+	q := u.Query()
+
+	q.Set("_count", strconv.Itoa(count))
+	q.Set("_offset", strconv.Itoa(offset))
+
+	u.RawQuery = q.Encode()
+	return u.String()
 }
 
 func (s *patientService) InsertPatient(ctx context.Context, p fhir.Patient) (paciente, error) {
@@ -39,4 +54,37 @@ func (s *patientService) InsertPatient(ctx context.Context, p fhir.Patient) (pac
 	}
 
 	return pt, nil
+}
+
+func (s *patientService) ListPatients(ctx context.Context, count, page int) (fhir.Bundle, error) {
+	pPage, err := s.repository.ListPatients(count, page)
+	if err != nil {
+		return fhir.Bundle{}, err
+	}
+
+	totalPages := int(math.Ceil(float64(pPage.Total) / float64(count)))
+
+	currentOffset := page * count
+	nextOffset := (page + 1) * count
+	previousOffset := (page - 1) * count
+
+	var links []fhir.BundleLink
+
+	links = append(links, fhir.BundleLink{
+		Relation: "self",
+		Url:      buildLinkURL("http://localhost:8090/Patient", count, page),
+	})
+
+	if page < totalPages-1 && len(pPage.Patients) == count {
+		links = append(links, fhir.BundleLink{
+			Relation: "next",
+			Url:      buildLinkURL("http://localhost:8090/Patient", count, nextOffset),
+		})
+	}
+
+	bundle := fhir.Bundle{
+		Type:  fhir.BundleTypeSearchset,
+		Total: &pPage.Total,
+		Link:  links,
+	}
 }
