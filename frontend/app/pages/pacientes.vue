741dcbe9d732928c6e9a950f68d0294bfcad0f6b
Samuel <samuelvitor224@gmail.com>
2025-09-26 18:02:55 -0300

    visuals overhaul

--- frontend/app/pages/pacientes.vue
+++ frontend/app/pages/pacientes.vue
@@ -2,16 +2,17 @@
 
 </script>
 
-
 <template>
-  <h1>Hello, World!</h1>
+  <div class="w-full flex justify-center mt-4">
+    <NuxtLink 
+      to="/cadastro" 
+      class="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-15 rounded-xl transition-all shadow-lg"
+    >
+      <b>Cadastrar Paciente</b>
+    </NuxtLink>
+  </div>
 </template>
 
-
-
 <style scoped>
-.container {
-  padding: 20px;
-}
-</style>
 
+</style>
