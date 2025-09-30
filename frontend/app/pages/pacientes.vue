f90c18441992bb4c65c5c636e22de9c6c73b0294
gui-henri <guihenrique.dev@gmail.com>
2025-09-30 18:18:37 -0300

    feat: initial table of patients

--- frontend/app/pages/pacientes.vue
+++ frontend/app/pages/pacientes.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { ref } from "vue";
+const config = useRuntimeConfig();
 
-const open = ref(false);
-
+const { data, pending, error, refresh } = await useAsyncData('Patient/all', () =>
+  $fetch('/Patient', {
+    baseURL: config.apiBase ?? "http://localhost:8090"
+  })
+)
 </script>
 
 <template>
@@ -13,10 +16,31 @@ const open = ref(false);
       <b>Cadastrar Paciente</b>
     </NuxtLink>
   </div>
-  <h1><b>PACIENTES APARECEM AQUI EM BAIXO</b> </h1>
+  <div v-if="pending" class="text-center text-gray-500">
+        <p>Carregando pacientes...</p>
+      </div>
+  <div v-else-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg text-center" role="alert">
+    <strong>Ocorreu um erro:</strong>
+    <p>{{ error.message }}</p>
+  </div>
+  <table v-else class="w-[896px]">
+    {{ data }}
+    <thead>
+      <tr>
+        <th class="text-start">Invoice</th>
+        <th class="text-start">Payment Status</th>
+        <th class="text-start">Payment Method</th>
+        <th class="text-start">Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="invoice in invoices" :key="invoice.invoice">
+        <td>{{ invoice.invoice }}</td>
+        <td>{{ invoice.paymentStatus }}</td>
+        <td>{{ invoice.paymentMethod }}</td>
+        <td>{{ invoice.totalAmount }}</td>
+      </tr>
+    </tbody>
+  </table>
 
 </template>
-
-<style scoped>
-
-</style>
