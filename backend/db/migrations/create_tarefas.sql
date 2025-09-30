b29bc3d33f17b5a51a1a5922d982888146d00f18
gui-henri <guihenrique.dev@gmail.com>
2025-09-30 16:33:15 -0300

    feat: modified tasks table to connect with patient

--- backend/db/migrations/create_tarefas.sql
+++ backend/db/migrations/create_tarefas.sql
@@ -1,31 +1,38 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS tasks (
-  id SERIAL PRIMARY KEY,
-  descricao TEXT NOT NULL,
-  prazo TEXT,
-  concluida BOOLEAN DEFAULT FALSE,
-  criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    descricao TEXT NOT NULL,
+    prazo TEXT,
+    concluida BOOLEAN DEFAULT FALSE,
+    patient_id VARCHAR,
+    criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP CONSTRAINT,
+    fk_patient Foreign Key (patient_id) REFERENCES patient (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS patient (
- internal_id BIGSERIAL PRIMARY KEY,
- id VARCHAR UNIQUE NOT NULL,
- last_updated TIMESTAMPTZ,
- active BOOLEAN DEFAULT TRUE,
- gender VARCHAR(20),      -- PROMOTED FIELD
- birth_date DATE,         -- PROMOTED FIELD
- deceased BOOLEAN,
- full_name VARCHAR(255),  -- PROMOTED FIELD
- cpf VARCHAR(11) UNIQUE,  -- PROMOTED FIELD
- created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
- resource_json JSONB NOT NULL
+    internal_id BIGSERIAL PRIMARY KEY,
+    id VARCHAR UNIQUE NOT NULL,
+    last_updated TIMESTAMPTZ,
+    active BOOLEAN DEFAULT TRUE,
+    gender VARCHAR(20), -- PROMOTED FIELD
+    birth_date DATE, -- PROMOTED FIELD
+    deceased BOOLEAN,
+    full_name VARCHAR(255), -- PROMOTED FIELD
+    cpf VARCHAR(11) UNIQUE, -- PROMOTED FIELD
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    resource_json JSONB NOT NULL
 );
 
-CREATE INDEX idx_patient_id ON patient(id);
-CREATE INDEX idx_patient_cpf ON patient(cpf);
-CREATE INDEX idx_patient_birth_date ON patient(birth_date);
-CREATE INDEX idx_patient_full_name ON patient USING GIN (to_tsvector('portuguese', full_name));
+CREATE INDEX idx_patient_id ON patient (id);
+
+CREATE INDEX idx_patient_cpf ON patient (cpf);
+
+CREATE INDEX idx_patient_birth_date ON patient (birth_date);
+
+CREATE INDEX idx_patient_full_name ON patient USING GIN (
+    to_tsvector('portuguese', full_name)
+);
 
 -- Índice GIN para buscas flexíveis dentro do JSONB (para campos não promovidos).
-CREATE INDEX idx_patient_recurso_gin ON patient USING GIN(resource_json);
+CREATE INDEX idx_patient_recurso_gin ON patient USING GIN (resource_json);
