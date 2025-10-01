84123901fd35ee73dbf3cf8b372d6b128949e28f
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-10-01 14:34:33 -0300

    feat: patient listing page

--- frontend/app/pages/pacientes.vue
+++ frontend/app/pages/pacientes.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Ellipsis, Eye, Trash2, BookCheck, Plus } from "lucide-vue-next"
 const config = useRuntimeConfig();
 
 const { data, pending, error, refresh } = await useAsyncData('Patient/all', () =>
@@ -10,39 +11,56 @@ const { data, pending, error, refresh } = await useAsyncData('Patient/all', () =
 
 <template>
   <div class="w-full flex justify-center mt-4">
-    <NuxtLink 
-      to="/cadastro" 
-      class="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-15 rounded-xl transition-all shadow-lg">
-      <b>Cadastrar Paciente</b>
-    </NuxtLink>
+    
   </div>
-  <div v-if="pending" class="text-center text-gray-500">
-        <p>Carregando pacientes...</p>
+  <div class="w-full flex flex-col items-center">
+    <div class="mt-2 flex flex-col items-end gap-6">
+      <div class="w-full flex justify-between">
+        <h1 class="font-bold text-3xl">Pacientes</h1>
+        <NuxtLink 
+          to="/cadastro" 
+        >
+          <Button class="bg-red-600 hover:bg-red-700"><Plus />Novo Paciente</Button>
+        </NuxtLink>
       </div>
-  <div v-else-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg text-center" role="alert">
-    <strong>Ocorreu um erro:</strong>
-    <p>{{ error.message }}</p>
+      <div v-if="pending" class="text-center text-gray-500">
+          <p>Carregando pacientes...</p>
+        </div>
+      <div v-else-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg text-center" role="alert">
+        <strong>Ocorreu um erro:</strong>
+        <p>{{ error.message }}</p>
+      </div>
+      <table v-else class="w-[1024px] pl-4 pr-4 pt-3 pb-3 border-2 table-auto border-t-red-400 border-l-red-400 border-e-red-600 border-b-red-600 rounded-2xl border-separate">
+        <thead>
+          <tr>
+            <th class="text-start border-b-2 border-red-300"><Button class="bg-white text-black hover:bg-red-300">Nome</Button></th>
+            <th class="text-start border-b-2 border-red-300"><Button class="bg-white text-black hover:bg-red-300">CPF</Button></th>
+            <th class="text-start border-b-2 border-red-300"><Button class="bg-white text-black hover:bg-red-300">Data de Nascimento</Button></th>
+            <th class="text-start border-b-2 border-red-300"><Button class="bg-white text-black hover:bg-red-300">Gênero</Button></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-2 border-red-600" v-for="(patient, index) in data.data.entry" :key="index">
+            <td class="p-2">{{ (patient.resource.name.find(n => n.use === "official" || n.use === "usual")?.given || ["Sem nome"]).join(" ") + patient.resource.name.find(n => n.use === "official" || n.use === "usual")?.family }}</td>
+            <td class="p-2">{{ (patient.resource.identifier.find(t => t.system ==="http://hl7.org.br/fhir/r4/sid/CPF")?.value) || "Sem CPF" }}</td>
+            <td class="p-2">{{ (patient.resource.birthDate) || "Sem Data" }}</td>
+            <td class="p-2">{{ (patient.resource.gender === "male" ? "Masculino" : patient.resource.gender === "female" ? "Feminino" : "Outro") }}</td>
+            <td class="flex gap-1 justify-center items-center">
+              <Popover>
+                <PopoverTrigger>
+                  <Button class="bg-red-300 text-red-800 hover:bg-red-400"><Ellipsis /></Button>
+                </PopoverTrigger>
+                <PopoverContent class="flex flex-col gap-2 w-36 border-2 border-red-300">
+                  <Button class="w-28 flex justify-start bg-white text-gray-700 hover:bg-red-300"><Eye />Detalhes</Button>
+                  <Button class="w-28 flex justify-start bg-white text-green-700 hover:bg-red-300"><BookCheck />Tarefas</Button>
+                  <Button class="w-28 flex justify-start bg-white text-red-700 hover:bg-red-300"><Trash2 />Deletar</Button>
+                </PopoverContent>
+              </Popover>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   
-  <table v-else class="w-[896px]">
-    <thead>
-      <tr>
-        <th class="text-start">Nome</th>
-        <th class="text-start">Email</th>
-        <th class="text-start">CPF</th>
-        <th class="text-start">Data de Nascimento</th>
-        <th class="text-start">Gênero</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(patient, index) in data.data.entry" :key="index">
-        <td>{{ (patient.resource.name.find(n => n.use === "official" || n.use === "usual")?.given || ["Sem nome"]).join(" ") + patient.resource.name.find(n => n.use === "official" || n.use === "usual")?.family }}</td>
-        <td>{{ (patient.resource.telecom.find(t => t.system ==="email"))?.value || "Sem Email" }}</td>
-        <td>{{ (patient.resource.identifier.find(t => t.system ==="http://hl7.org.br/fhir/r4/sid/CPF")?.value) || "Sem CPF" }}</td>
-        <td>{{ (patient.resource.birthDate) || "Sem Data" }}</td>
-        <td>{{ (patient.resource.gender === "male" ? "Masculino" : patient.resource.gender === "female" ? "Feminino" : "Outro") }}</td>
-      </tr>
-    </tbody>
-  </table>
-
 </template>
