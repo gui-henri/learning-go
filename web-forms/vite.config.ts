39cf6355a7a8d2101331c680d1c7ba7805373b85
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-11-12 15:11:57 -0300

    feat: adding components

--- web-forms/vite.config.ts
+++ web-forms/vite.config.ts
@@ -8,6 +8,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:  {
+    watch: {
+      usePolling: true,     // 👈 enables polling
+      interval: 300,        // 👈 check for changes every 300ms
+    },
+  },
   plugins: [
     vue(),
     vueDevTools(),
