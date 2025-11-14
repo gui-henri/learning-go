a61d533d9227f04877860a0582acc6caf66a3f5d
Samuel <samuelvitor224@gmail.com>
2025-11-14 17:14:42 -0300

    early phone view

--- web-forms/src/views/uikit/FormLayout.vue
+++ web-forms/src/views/uikit/FormLayout.vue
@@ -21,68 +21,75 @@ const complexidade = ref([
 
 <template>
     <Fluid >
-        <div class="card shadow-2xl rounded-2xl w-full sm:p-8 border-t-8 border-red-600">
+        <div class="card shadow-2xl rounded-2xl w-full p-4 sm:p-8 border-t-8 border-red-600">
             <div class="flex flex-col gap-4 w-full">
                 <h4 class="font-semibold text-xl">Dados Gerais</h4>
+
                 <div class="flex flex-col md:flex-row gap-4">
-                    <div class="flex flex-wrap gap-2 w-full">
-                        <label for="lastname2">Nome completo</label>
-                        <InputText id="lastname2" v-model="store.evaluation.nome_paciente" type="text" placeholder="Glauberthy júnior"/>
+                    <div class="flex flex-col gap-2 w-full">
+                        <label for="nome_paciente">Nome completo</label>
+                        <InputText id="nome_paciente" v-model="store.evaluation.nome_paciente" type="text" placeholder="Glauberthy júnior"/>
                     </div>
                 </div>
 
-                <div class="flex flex-col md:flex-row gap-4 flex-wrap">
-                    <div class="flex flex-wrap gap-2 lg:w-40">
-                        <label for="state">Gênero</label>
-                        <Select id="state" v-model="store.evaluation.genero" :options="genero" optionLabel="name" placeholder="Selecione" class="w-full"></Select>
+                <div class="flex flex-col md:flex-row gap-4">
+                    <div class="flex flex-col gap-2  md:w-1/2">
+                        <label for="genero">Gênero</label>
+                        <Select id="genero" v-model="store.evaluation.genero" :options="genero" optionLabel="name" placeholder="Selecione" class="w-full"></Select>
                     </div>
-                    <div class="flex gap-4">
-                        <div class="flex flex-wrap gap-2 w-24">
-                            <label for="zip">Peso</label>
-                            <InputGroup>
-                                <InputText id="weight" v-model="store.evaluation.peso" type="text" maxlength="3" placeholder="64"/>
-                                <InputGroupAddon>kg</InputGroupAddon>
-                            </InputGroup>
-                        </div>
-
-                        <div class="flex flex-wrap gap-2 w-24">
-                            <label for="zip">Altura</label>
-                            <InputGroup>
-                                <InputText id="height" v-model="store.evaluation.altura" type="text" maxlength="3" placeholder="181"/>
-                                <InputGroupAddon>cm</InputGroupAddon>
-                            </InputGroup>
-                        </div>
-                        <div class="flex flex-wrap gap-2 w-28">
-                            <label for="zip">Idade</label>
-                            <InputGroup>
-                                <InputText id="age" v-model="store.evaluation.idade" type="text" maxlength="3" placeholder="32"/>
-                                <InputGroupAddon>anos</InputGroupAddon>
-                            </InputGroup>
-                        </div>
-                        <div class="flex flex-col gap-2"> <label for="zip">Previsão de alta?</label>
-                         <InputSwitch v-model="store.evaluation.previsao_alta" />
-                    </div>
-                    <div class="flex flex-wrap gap-2 lg:w-40">
-                        <label for="state">Complexidade</label>
-                        <Select id="state" v-model="store.evaluation.complexidade" :options="complexidade" optionLabel="name" placeholder="Selecione" class="w-full"></Select>
-                    </div>
-                </div>
-              
-                <div class="flex flex-col md:flex-row gap-4 flex-wrap" >
-                    <div class="flex flex-wrap gap-2 lg:w-40">
-                        <label for="zip">Data de nascimento</label>
+                    <div class="flex flex-col gap-2 w-full md:w-1/2">
+                        <label for="birthdate">Data de nascimento</label>
                         <Calendar id="birthdate" dateFormat="dd/mm/yy" placeholder="dd/mm/aaaa" class="w-full"/>
                     </div>
                 </div>
 
-                <div class="flex flex-wrap gap-2 w-full">
-                    <label for="lastname2">Nome do pai</label>
-                    <InputText id="lastname2" type="text" placeholder="Glauberthy glauberthy"/>
+                <div class="flex flex-col md:flex-row gap-4">
+                    <div class="flex flex-col gap-2 w-full md:w-1/3">
+                        <label for="weight">Peso</label>
+                        <InputGroup>
+                            <InputText id="weight" v-model="store.evaluation.peso" type="text" maxlength="3" placeholder="64"/>
+                            <InputGroupAddon>kg</InputGroupAddon>
+                        </InputGroup>
+                    </div>
+                    <div class="flex flex-col gap-2 w-full md:w-1/3">
+                        <label for="height">Altura</label>
+                        <InputGroup>
+                            <InputText id="height" v-model="store.evaluation.altura" type="text" maxlength="3" placeholder="181"/>
+                            <InputGroupAddon>cm</InputGroupAddon>
+                        </InputGroup>
+                    </div>
+                    <div class="flex flex-col gap-2 w-full md:w-1/3">
+                        <label for="age">Idade</label>
+                        <InputGroup>
+                            <InputText id="age" v-model="store.evaluation.idade" type="text" maxlength="3" placeholder="32"/>
+                            <InputGroupAddon>anos</InputGroupAddon>
+                        </InputGroup>
+                    </div>
                 </div>
-                <div class="flex flex-wrap gap-2 w-full">
-                    <label for="lastname2">Nome da mãe</label>
-                    <InputText id="lastname2" type="text" placeholder="Júlia silva"/>
+
+                <div class="flex flex-col md:flex-row gap-4 md:items-end">
+                    <div class="flex flex-col gap-2 w-full md:w-1/2">
+                        <label for="complexidade">Complexidade</label>
+                        <Select id="complexidade" v-model="store.evaluation.complexidade" :options="complexidade" optionLabel="name" placeholder="Selecione" class="w-full"></Select>
+                    </div>
+                    <div class="flex flex-col gap-2 py-2"> 
+                        <label for="previsao_alta">Previsão de alta?</label>
+                        <InputSwitch id="previsao_alta" v-model="store.evaluation.previsao_alta" />
+                    </div>
                 </div>
+            
+                <div class="flex flex-col md:flex-row gap-4">
+                    <div class="flex flex-col gap-2 w-full">
+                        <label for="nome_pai">Nome do pai</label>
+                        <InputText id="nome_pai" type="text" placeholder="Glauberthy glauberthy"/>
+                    </div>
+                </div>
+
+                <div class="flex flex-col md:flex-row gap-4">
+                    <div class="flex flex-col gap-2 w-full">
+                        <label for="nome_mae">Nome da mãe</label>
+                        <InputText id="nome_mae" type="text" placeholder="Júlia silva"/>
+                    </div>
                 </div>
             </div>
         </div>
