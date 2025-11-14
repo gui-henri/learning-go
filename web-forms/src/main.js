a085a841ee4471c5dba845692faeacf605c5c218
Guilherme Henrique Freitas da Silva <guilherme.freitas@interne.com.br>
2025-11-14 14:18:00 -0300

    feat: color and top bar updated

--- web-forms/src/main.js
+++ web-forms/src/main.js
@@ -6,6 +6,7 @@ import Aura from '@primeuix/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
+import { definePreset } from '@primeuix/themes';
 
 import '@/assets/tailwind.css';
 import '@/assets/styles.scss';
@@ -13,9 +14,16 @@ import '@/assets/styles.scss';
 const app = createApp(App);
 
 app.use(router);
+
+const Interne = definePreset(Aura, {
+    semantic: {
+        primary: { 0: '#ffffff', 50: '#ffeeef', 100: '#ffdadb', 200: '#ffbbbd', 300: '#ff8b8f', 400: '#ff4950', 500: '#ff111a', 600: '#ff0009', 700: '#e70008', 800: '#be0007', 900: '#a80006', 950: '#560003' }
+    }
+});
+
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: Interne,
         options: {
             darkModeSelector: '.app-dark'
         }
