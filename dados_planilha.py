c9cf18ad687021d045f14a559a6364265229f7fd
Guilherme Henrique <guihenrique.dev@gmail.com>
2025-11-18 17:56:14 -0300

    feat: mapping sheet up until gross revenue

---
+++ dados_planilha.py
@@ -0,0 +1,426 @@
+orcamento = [
+    {
+        "descricao": "HOME CARE DIARIA COM ALTA COMPLEXIDADE",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "785,0000",
+        "valor_total": "14.130,00",
+        "grupo_procedimento": "DIARIAS"
+    },
+    {
+        "descricao": "TAXA DE CONCENTRADOR DE OXIGENIO, POR USO",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "11,1800",
+        "valor_total": "201,24",
+        "grupo_procedimento": "TAXAS"
+    },
+    {
+        "descricao": "COUGH ASSIST - DIA",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "40,2400",
+        "valor_total": "724,32",
+        "grupo_procedimento": "TAXAS"
+    },
+    {
+        "descricao": "TAXA RESPIRADOR TRILOGY",
+        "quantidade": 18.0,
+        "unidade": "DIA",
+        "valor_unitario": "0,0100",
+        "valor_total": "0,18",
+        "grupo_procedimento": "TAXAS"
+    },
+    {
+        "descricao": "VISITA DA ENFERMEIRA - VISITA",
+        "quantidade": 1.0,
+        "unidade": "UN",
+        "valor_unitario": "85,0000",
+        "valor_total": "85,00",
+        "grupo_procedimento": "SERV ENFERMAGEM"
+    },
+    {
+        "descricao": "FISIOTERAPIA MOTORA E RESPIRATORIA - SESSAO",
+        "quantidade": 8.0,
+        "unidade": "UN",
+        "valor_unitario": "53,0900",
+        "valor_total": "424,72",
+        "grupo_procedimento": "SERVICOS DE FISIOTERAPIA"
+    },
+    {
+        "descricao": "SESSAO INDIVIDUAL DOMICILIAR DE FONOAUDIOLOGIA",
+        "quantidade": 4.0,
+        "unidade": "UN",
+        "valor_unitario": "54,6400",
+        "valor_total": "218,56",
+        "grupo_procedimento": "SERVICOS DE FONOAUDIOLOGIA"
+    },
+    {
+        "descricao": "VISITA DO NUTRICIONISTA (AVALIACAO)",
+        "quantidade": 1.0,
+        "unidade": "UN",
+        "valor_unitario": "0,0100",
+        "valor_total": "0,01",
+        "grupo_procedimento": "SERVICOS DE NUTRICAO"
+    },
+    {
+        "descricao": "VISITA TERAPIA OCUPACIONAL",
+        "quantidade": 3.0,
+        "unidade": "UN",
+        "valor_unitario": "58,0300",
+        "valor_total": "174,09",
+        "grupo_procedimento": "SERVICOS DE TERAPIA OCUPACIONAL"
+    },
+    {
+        "descricao": "VISITA MEDICA DOMICILIAR",
+        "quantidade": 2.0,
+        "unidade": "UN",
+        "valor_unitario": "143,9300",
+        "valor_total": "287,86",
+        "grupo_procedimento": "VISITAS E CONSULTAS MEDICAS - HC"
+    },
+    {
+        "descricao": "CLOR. DE VENLAFAXINA 75MG CPRS",
+        "quantidade": 18.0,
+        "unidade": "CAP",
+        "valor_unitario": "2,6600",
+        "valor_total": "47,88",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "DOMPERIDONA 10MG CPRS",
+        "quantidade": 54.0,
+        "unidade": "COM",
+        "valor_unitario": "0,6000",
+        "valor_total": "32,40",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "PANTOPRAZOL CPRS 40MG",
+        "quantidade": 18.0,
+        "unidade": "COM",
+        "valor_unitario": "2,5800",
+        "valor_total": "46,44",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "PATZ SL 5MG CPRS",
+        "quantidade": 18.0,
+        "unidade": "COM",
+        "valor_unitario": "2,2700",
+        "valor_total": "40,86",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "PEG-LAX PO C/14 ENVELOPES SEM SABOR 8.5G",
+        "quantidade": 15.0,
+        "unidade": "UN",
+        "valor_unitario": "2,2900",
+        "valor_total": "34,35",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "ROSUVASTATINA CALCICA",
+        "quantidade": 18.0,
+        "unidade": "COM",
+        "valor_unitario": "5,0400",
+        "valor_total": "90,72",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "SIMETICONA 75MG/ML FRAS/ 10ML",
+        "quantidade": 3510.0,
+        "unidade": "GTS",
+        "valor_unitario": "0,0300",
+        "valor_total": "105,30",
+        "grupo_procedimento": "MEDICAMENTOS"
+    },
+    {
+        "descricao": "AGUA P/ INJECAO AMP 10ML",
+        "quantidade": 18.0,
+        "unidade": "AMP",
+        "valor_unitario": "0,8300",
+        "valor_total": "14,94",
+        "grupo_procedimento": "MEDICAMENTOS RESTRITO HOSP."
+    },
+    {
+        "descricao": "AGUA PARA INJECAO",
+        "quantidade": 21.0,
+        "unidade": "BOLS",
+        "valor_unitario": "8,6000",
+        "valor_total": "180,60",
+        "grupo_procedimento": "MEDICAMENTOS RESTRITO HOSP."
+    },
+    {
+        "descricao": "CLOR. DE SODIO 0,9% AMP 10ML (FARMACE)",
+        "quantidade": 90.0,
+        "unidade": "AMP",
+        "valor_unitario": "1,0500",
+        "valor_total": "94,50",
+        "grupo_procedimento": "MEDICAMENTOS RESTRITO HOSP."
+    },
+    {
+        "descricao": "CLORETO DE SODIO",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "8,3000",
+        "valor_total": "149,40",
+        "grupo_procedimento": "MEDICAMENTOS RESTRITO HOSP."
+    },
+    {
+        "descricao": "PIPERACILINA SODICA + TAZOBACTAM 4,5MG (NOVAFARMA)",
+        "quantidade": 18.0,
+        "unidade": "FA",
+        "valor_unitario": "142,2000",
+        "valor_total": "2.559,60",
+        "grupo_procedimento": "MEDICAMENTOS RESTRITO HOSP."
+    },
+    {
+        "descricao": "FRESUBIN HP ENERGY 1000ML (FRESENIUS) - ORC",
+        "quantidade": 17100.0,
+        "unidade": "ML",
+        "valor_unitario": "0,0000",
+        "valor_total": "0,00",
+        "grupo_procedimento": "ALIMENTACOES E DIETAS"
+    },
+    {
+        "descricao": "AGULHA DESCARTAVEL 13X4,5MM",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "1,0600",
+        "valor_total": "19,08",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "AGULHA DESCARTAVEL 25X7MM (0097046)",
+        "quantidade": 72.0,
+        "unidade": "UN",
+        "valor_unitario": "2,3300",
+        "valor_total": "167,76",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "AGULHA DESCARTAVEL 40X12MM",
+        "quantidade": 6.0,
+        "unidade": "UN",
+        "valor_unitario": "2,3300",
+        "valor_total": "13,98",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "ALCOOL 70% FRASCO 100(ML)",
+        "quantidade": 6.0,
+        "unidade": "ML",
+        "valor_unitario": "0,1000",
+        "valor_total": "0,60",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "ALCOOL SWABS BD SACHE (770131)",
+        "quantidade": 24.0,
+        "unidade": "UN",
+        "valor_unitario": "1,4500",
+        "valor_total": "34,80",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "BORRACHA LATEX 2,0M",
+        "quantidade": 21.0,
+        "unidade": "UN",
+        "valor_unitario": "111,9000",
+        "valor_total": "2.349,90",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "CAIXA COLETORA BOX NR 03 PERFURANTE/CORTANTE (0101830)",
+        "quantidade": 3.0,
+        "unidade": "UN",
+        "valor_unitario": "4,3300",
+        "valor_total": "12,99",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "CLOREXIDINA 0,5% ALMOT 100ML",
+        "quantidade": 60.0,
+        "unidade": "ML",
+        "valor_unitario": "0,0600",
+        "valor_total": "3,60",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "COMPRESSA GAZE 11FIOS 7,5X7,5CM 10 UNID EST.",
+        "quantidade": 96.0,
+        "unidade": "PC",
+        "valor_unitario": "7,5000",
+        "valor_total": "720,00",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "DISPOSITIVO DE FIXACAO STATLOCK PICC PLUS/POSTE FIXO (56140)",
+        "quantidade": 3.0,
+        "unidade": "UN",
+        "valor_unitario": "158,6300",
+        "valor_total": "475,89",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "DISPOSITIVO FREESITE SISTEMA FECHADO (EMBRAMED)",
+        "quantidade": 6.0,
+        "unidade": "UN",
+        "valor_unitario": "43,7500",
+        "valor_total": "262,50",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "EQUIPO P/ NUTRICAO ENTERAL ESCALONADO",
+        "quantidade": 3.0,
+        "unidade": "UN",
+        "valor_unitario": "45,5500",
+        "valor_total": "136,65",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "EQUIPO P/ SORO PINCA MACRO EMBRAMED",
+        "quantidade": 6.0,
+        "unidade": "UN",
+        "valor_unitario": "51,0000",
+        "valor_total": "306,00",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "EQUIPO P/BOMBA INF KANGAROO ENTERAL E-PUMP (0261767)",
+        "quantidade": 9.0,
+        "unidade": "UN",
+        "valor_unitario": "850,0000",
+        "valor_total": "7.650,00",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "FILTRO AUTO-UMID.BARREIRA BACTERIA VIRUS PALL P/CTI/CC BAIXO VOLUME",
+        "quantidade": 6.0,
+        "unidade": "UN",
+        "valor_unitario": "222,1400",
+        "valor_total": "1.332,84",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "FIXADOR P/CANULA TRAQ. ADU. TRACH-FIX (0044862)",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "77,5000",
+        "valor_total": "1.395,00",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "FRALDA TENA SLIP ULTRA G (31899)",
+        "quantidade": 108.0,
+        "unidade": "UN",
+        "valor_unitario": "5,2000",
+        "valor_total": "561,60",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "FRASCO P/NUTRICAO ENTERAL NUTRIZ 300ML (0106816)",
+        "quantidade": 21.0,
+        "unidade": "UN",
+        "valor_unitario": "6,2500",
+        "valor_total": "131,25",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "LUVA P/PROCEDIMENTO NAO ESTERIL TAM M",
+        "quantidade": 237.0,
+        "unidade": "UN",
+        "valor_unitario": "0,8500",
+        "valor_total": "201,45",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "LUVA PROCEDIMENTO ESTERIL MED.(PAR) (0008256)",
+        "quantidade": 147.0,
+        "unidade": "UN",
+        "valor_unitario": "2,1800",
+        "valor_total": "320,46",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "MASCARA N95 DESCARPACK (44628)",
+        "quantidade": 36.0,
+        "unidade": "UN",
+        "valor_unitario": "9,0000",
+        "valor_total": "324,00",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "MICROPORE 50MM X 10MT 3M",
+        "quantidade": 540.0,
+        "unidade": "CM",
+        "valor_unitario": "0,0600",
+        "valor_total": "32,40",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "SACO BRANCO PARA LIXO INFECTADO 40LTS",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "0,2000",
+        "valor_total": "3,60",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "SERINGA 10ML S/AGUL. DESC",
+        "quantidade": 54.0,
+        "unidade": "UN",
+        "valor_unitario": "4,7800",
+        "valor_total": "258,12",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "SERINGA 20ML S/AGUL. DESC. (INJEX) - (0074275)",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "14,0900",
+        "valor_total": "253,62",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "SERINGA 5ML S/AGUL. DESC.",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "3,5500",
+        "valor_total": "63,90",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "SONDA ASPIRACAO TRAQUEAL N.10 (2340035)",
+        "quantidade": 108.0,
+        "unidade": "UN",
+        "valor_unitario": "11,0600",
+        "valor_total": "1.194,48",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "TIRA REAGENTE ON CALL PLUS MEDLEVENSOHN (288785)",
+        "quantidade": 18.0,
+        "unidade": "UN",
+        "valor_unitario": "3,8000",
+        "valor_total": "68,40",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "TOUCA DESCARTAVEL",
+        "quantidade": 36.0,
+        "unidade": "UN",
+        "valor_unitario": "1,0800",
+        "valor_total": "38,88",
+        "grupo_procedimento": "MATERIAIS"
+    },
+    {
+        "descricao": "CURATIVO TEGADERM CHG 8,5X11,5CM",
+        "quantidade": 3.0,
+        "unidade": "UN",
+        "valor_unitario": "137,3200",
+        "valor_total": "411,96",
+        "grupo_procedimento": "CURATIVOS ESPECIAIS"
+    }
+]
