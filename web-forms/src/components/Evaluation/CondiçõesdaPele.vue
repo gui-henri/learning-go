69c41b38bcbac81214db587805ab83dd73a1e536
Samuel <samuelvitor224@gmail.com>
2025-11-26 13:12:32 -0300

    forms lower half

--- web-forms/src/components/Evaluation/CondiçõesdaPele.vue
+++ web-forms/src/components/Evaluation/CondiçõesdaPele.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useCondicoesPeleStore } from '@/store/evaluation/condicoesPele';
+
+const emit = defineEmits(['next-step']);
 
 const condicoesPeleStore = useCondicoesPeleStore();
 
@@ -9,14 +11,48 @@ const condicaoCutaneaOpts = ref([
     { name: 'Com Lesão', code: 'lesao' }
 ]);
 
+const activeIndex = ref(null);
+
+const isFilled = computed(() => {
+    return !!condicoesPeleStore.condicoesPele.condicao_cutanea_mucosa;
+});
+
+const handleSave = () => {
+    activeIndex.value = null;
+    
+    setTimeout(() => {
+        const self = document.getElementById("condicoesPele");
+        if (self) {
+            self.scrollIntoView({ 
+                behavior: 'instant', 
+                block: 'start',
+            });
+        }
+    }, 0);
+    emit('next-step');
+};
+
+
+
 </script>
-
 <template>
-    <div class="card shadow-2xl rounded-2xl w-full p-4 sm:p-8 border-t-8 border-red-600 mb-8 mt-8">
+    <Accordion v-model:activeIndex="activeIndex" id="condicoesPele" class="scroll-mt-24 card shadow-2xl rounded-2xl w-full p-4 sm:p-8 border-t-8 border-red-600">
+        <AccordionTab>
+        <template #header>
+            <div class="flex items-center gap-3 w-full">
+                <i class="pi text-xl" 
+                    :class="isFilled ? 'pi-check-circle text-green-600' : 'pi-plus-circle text-gray-400'">
+            </i>
+            <div class="flex flex-col text-left">
+                <h4 class="font-semibold text-xl p-0 m-0">Condições da Pele</h4>
+                <span class="text-xs text-gray-500 font-normal -mt-4">
+                    {{ isFilled ? '' : 'Toque para preencher' }}
+                </span>
+            </div>
+        </div>
+    </template>
         <div class="flex flex-col gap-4 w-full">
-            
-            <h4 class="font-semibold text-xl">Condições da Pele</h4>
-
+        
             <div class="flex flex-col md:flex-row gap-4">
                 <div class="flex flex-col gap-2 w-full md:w-1/3">
                     <label for="condicao_cutanea_mucosa">Como é a Cutânea-mucosa</label>
@@ -101,7 +137,11 @@ const condicaoCutaneaOpts = ref([
                     </div>
                 </div>
             </div>
-
         </div>
-    </div>
+        <Button class="mt-3" v-on:click="handleSave">
+            <i class="pi text-xl" :class="'pi-check-circle text-white dark:text-black'" />
+            Próximo
+        </Button>
+    </AccordionTab>
+</Accordion>
 </template>
