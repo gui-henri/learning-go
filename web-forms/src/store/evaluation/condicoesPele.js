99b400994b286170536d81c3cc57b7e67a9e57f5
Samuel <samuelvitor224@gmail.com>
2025-11-27 16:09:05 -0300

    overall structure

--- web-forms/src/store/evaluation/condicoesPele.js
+++ web-forms/src/store/evaluation/condicoesPele.js
@@ -1,30 +1,39 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-export const useCondicoesPeleStore = defineStore('condicoesPele', {
-    state: () => ({
-        condicoesPele: {
-            condicao_cutanea_mucosa: null,
-            realiza_curativo: false,
-            local_curativo: '',
-            tipo_cobertura: '',
-            tamanho_curativo: '',
-            qtd_curativo: '',
-            frequencia_troca: '',
-            materiais_curativo: ''
-        }
-    }),
-    actions: {
-        clean() {
-            this.condicoesPele = {
-                condicao_cutanea_mucosa: null,
-                realiza_curativo: false,
-                local_curativo: '',
-                tipo_cobertura: '',
-                tamanho_curativo: '',
-                qtd_curativo: '',
-                frequencia_troca: '',
-                materiais_curativo: ''
-            };
+const curativoBase = {
+    local_curativo: '',
+    tipo_cobertura: '',
+    tamanho_curativo: '',
+    qtd_curativo: '',
+    frequencia_troca: '',
+    materiais_curativo: ''
+};
+
+export const useCondicoesPeleStore = defineStore('condicoesPele', () => {
+
+    const condicoesPele = ref({
+        condicao_cutanea_mucosa: null,
+        realiza_curativo: false,
+        curativos: [] 
+    });
+
+    function adicionarCurativo() {
+
+        condicoesPele.value.curativos.push({ ...curativoBase });
+    }
+
+    function removerCurativo(index) {
+        if (condicoesPele.value.curativos.length > 0) {
+            condicoesPele.value.curativos.splice(index, 1);
         }
     }
+
+    return {
+        condicoesPele,
+        adicionarCurativo,
+        removerCurativo
+    };
+}, {
+    persist: true
 });
