d68b8656ff45024395a1edee1ac5ca3f8697f43d
gui-henri <guihenrique.dev@gmail.com>
2025-12-04 14:45:19 -0300

    fix: observations not being saved. All select fields working now

--- web-forms/src/store/evaluation/observacao.js
+++ web-forms/src/store/evaluation/observacao.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 
 export const useObservacoesStore = defineStore('observacoes', {
+    persist: true,
     state: () => ({
         observacoes: {
             texto: ''
